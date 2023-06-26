1d328e63cdf3d132e6212fcc10d17e86a82c2790
Yaswanth <ya8wanth@gmail.com>
2023-06-26 11:56:56 +0530

    Add simple routing for Homescreen and HotelDetail components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Layouts/Navbar/Navbar";
 import Homescreen from "./Components/HomeScreen/Homescreen";
 import "./App.css";
@@ -12,14 +13,21 @@ function App() {
   };
 
   return (
-    <div>
-      <Navbar
-        title="Presidio Bookings"
-        onAuthenticateClick={handleAuthenticateClick}
-      />
-      {/* <Homescreen isAuthVisible={isAuthVisible} /> */}
-      <HotelDetails />
-    </div>
+    <Router>
+      <div>
+        <Navbar
+          title="Presidio Bookings"
+          onAuthenticateClick={handleAuthenticateClick}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={<Homescreen isAuthVisible={isAuthVisible} />}
+          />
+          <Route path="/hotel" element={<HotelDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
