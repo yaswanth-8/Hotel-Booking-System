fee2a071e3fbdcfcb212e54d53afda6ec8d0721b
Yaswanth <ya8wanth@gmail.com>
2023-06-30 20:50:19 +0530

    Add basic responsiveness on Hotel_details page

--- frontend/src/Components/Hotels/Hotel-Detail/HotelDetail.jsx
+++ frontend/src/Components/Hotels/Hotel-Detail/HotelDetail.jsx
@@ -5,6 +5,8 @@ import Booking from "../../Layouts/Booking/Booking";
 import EditHotelDetails from "./Edit-Hotel-Detail/EditHotelDetails";
 import { useNavigate } from "react-router-dom";
 import useGetWeather from "../../../Hooks/useGetWeather";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function HotelDetails() {
   const hotel = useSelector((state) => state.hotel);
@@ -24,55 +26,58 @@ function HotelDetails() {
   };
 
   return (
-    <div className="hotelDetails-details">
-      <h2 className="hotelDetails-name">{hotel.Name}</h2>
-      <div className="hotel-details-booking">
-        <div className="hotel-details-data">
-          <div className="image-grid">
-            <div className="big-image">
-              <img src={hotel.Url} alt={hotel.Name} />
-            </div>
-            <div className="small-images">
-              {[...Array(4)].map((_, index) => (
-                <img key={index} src={hotel.Url} alt={hotel.Name} />
-              ))}
-            </div>
-          </div>
-
-          <div className="hotelDetails">
-            <p>{hotel.About}</p>
-            <p>
-              <strong>Address:</strong> {hotel.Address}
-            </p>
-            <p>
-              <strong>Location:</strong> {hotel.Location}
-            </p>
-            <p>
-              <strong>Country:</strong> {hotel.Country}
-            </p>
-            <p>
-              <strong>Current Weather:</strong> {weather} °C
-            </p>
-            <p>
-              <strong>Rating:</strong> {hotel.Rating}
-            </p>
-            <p>
-              <strong>Description:</strong> {hotel.Description}
-            </p>
-            <p>
-              <strong>Price Per Night:</strong> {hotel.PricePerNight}
-            </p>
-            <p>
-              <strong>Offer:</strong> {hotel.Offer}% off
-            </p>
-            <p>
-              <strong>Site:</strong> {hotel.Site}
-            </p>
-          </div>
-          <button onClick={showReviewsHandler}>Reviews</button>
-          <button onClick={raiseQueryHandler}>Raise-Query</button>
+    <div className="hotel-details-details">
+      <div className="hotel-details-data">
+        <h2 className="hotel-details-name">{hotel.Name}</h2>
+        <div className="image-grid">
+          <Carousel showThumbs={false}>
+            <img src={hotel.Url} alt={hotel.Name} />
+            <img src={hotel.Url} alt={hotel.Name} />
+            <img src={hotel.Url} alt={hotel.Name} />
+            <img src={hotel.Url} alt={hotel.Name} />
+            <img src={hotel.Url} alt={hotel.Name} />
+          </Carousel>
         </div>
 
+        <div className="hotelDetails">
+          <p>{hotel.About}</p>
+          <p>
+            <strong>Address:</strong> {hotel.Address}
+          </p>
+          <p>
+            <strong>Location:</strong> {hotel.Location}
+          </p>
+          <p>
+            <strong>Country:</strong> {hotel.Country}
+          </p>
+          <p>
+            <strong>Current Weather:</strong> {weather} °C
+          </p>
+          <p>
+            <strong>Rating:</strong> {hotel.Rating}
+          </p>
+          <p>
+            <strong>Description:</strong> {hotel.Description}
+          </p>
+          <p>
+            <strong>Price Per Night:</strong> {hotel.PricePerNight}
+          </p>
+          <p>
+            <strong>Offer:</strong> {hotel.Offer}% off
+          </p>
+          <p>
+            <strong>Site:</strong> {hotel.Site}
+          </p>
+        </div>
+        <button className="blue-button" onClick={showReviewsHandler}>
+          Reviews
+        </button>
+        <button className="blue-button" onClick={raiseQueryHandler}>
+          Raise-Query
+        </button>
+      </div>
+
+      <div className="hotel-details-booking">
         {auth === "admin" ? (
           <EditHotelDetails hotel={hotel} />
         ) : (
