7ee3f5a7db97471217dd1a6ac9b2754371467c4c
yaswanth <ya8wanth@gmail.com>
2023-07-03 13:32:33 +0530

    Add auto migration

--- backend/Data/backendContext.cs
+++ backend/Data/backendContext.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using backend.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace backend.Data
 {
@@ -12,6 +14,19 @@ namespace backend.Data
         public backendContext (DbContextOptions<backendContext> options)
             : base(options)
         {
+            try
+            {
+                var databaseCreater = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreater != null)
+                {
+                    if (!databaseCreater.CanConnect()) databaseCreater.Create();
+                    if (!databaseCreater.HasTables()) databaseCreater.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public DbSet<backend.Models.User> User { get; set; } = default!;
